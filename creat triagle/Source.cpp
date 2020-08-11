c462fab589e707515574e815f28805157a5157cb
PLEUM Supakiart <oyashi11110@gmail.com>
2020-08-11 15:15:03 +0700

    add all Code

--- creat triagle/Source.cpp
+++ creat triagle/Source.cpp
@@ -1,1 +1,15 @@
-//test
+#include <stdio.h>
+int main()
+{
+	int level;
+	scanf_s("%d", &level);
+	for (int row = 0; row < level; row++)
+	{
+		for (int star = 0; star <= row; star++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
